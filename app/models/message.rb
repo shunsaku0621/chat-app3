be286801bfd37fe1af9ebef86a17cbff21651b6a
shunsaku0621 <hamagi.0621@icloud.com>
2021-05-21 10:59:23 +0900

    画像のみでもメッセージ送信可能となるバリデーションの設定

--- app/models/message.rb
+++ app/models/message.rb
@@ -2,5 +2,10 @@ class Message < ApplicationRecord
   belongs_to :user
   belongs_to :room
   has_one_attached :image
-  validates :content, presence: true
+  validates :content, presence: true, unless: :was_at?
+
+
+  def was_at?
+    self.image.attachd?
+  end
 end
